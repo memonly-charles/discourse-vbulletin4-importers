6a857797361a64f9dc0c0c369efcb73356f8155c
memonly-charles <31261542+memonly-charles@users.noreply.github.com>
2017-08-27 23:18:34 +0900

    short comments on normalizations

--- vbulletin_permalink.rb
+++ vbulletin_permalink.rb
@@ -1,3 +1,18 @@
+# Add the following "permalink normalizations" in Admin settings to get these redirects to work.
+#
+# Example 1
+# http://your.vbulletindomain.com/forums/f10/some-thread-title-here-51689/index1.html
+# http://your.vbulletindomain.com/forums/f10/some-thread-title-here-51689/index15.html#323423
+#
+# Normalization 1. This is for the above 2 examples. Add this one first (order of normalizations is important!)
+# /(forums)\/f[0-9]+\/.+-([0-9]+)\/index[0-9]*.html/\1\2
+#
+# Example 2
+# http://your.vbulletindomain.com/forums/f10/some-thread-title-here-51689/
+#
+# Normalization 2. This is for the above example. Add this one first (order of normalizations is important!)
+# /(forums)\/f[0-9]+\/.+-([0-9]+)/\1\2
+
 require File.expand_path(File.dirname(__FILE__) + "/base.rb")
 
 # See https://meta.discourse.org/t/importing-from-vbulletin-4/54881
